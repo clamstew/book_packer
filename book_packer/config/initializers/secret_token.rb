65e09da7e9c44cee8c922da1a6adfbd1246d3716
bangner <joswagne@gmail.com>
2013-11-21 14:15:51 -0600

    first commit

---
+++ book_packer/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BookPacker::Application.config.secret_key_base = '1bb07b63daa553169f2dcab48dd0dfbf8a25df3191887a65fcd9daa32a8c738e81cb663a8413e3e8139f241df0ac7ac0a5f9386bb1f60348d98c16a9668c435e'
